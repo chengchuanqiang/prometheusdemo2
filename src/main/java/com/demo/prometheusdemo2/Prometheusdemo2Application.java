d4233b5598b625ee02d94eb0481e2778df0c062d
chengchuanqiang <1625320316@qq.com>
2018-10-08 17:03:28 +0800

    启动类设置公共标签

--- src/main/java/com/demo/prometheusdemo2/Prometheusdemo2Application.java
+++ src/main/java/com/demo/prometheusdemo2/Prometheusdemo2Application.java
@@ -1,10 +1,27 @@
 package com.demo.prometheusdemo2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Prometheusdemo2Application {
+
+    @Value("${project.name}")
+    private String projectName;
+
+    /**
+     * 设置全局tags变量
+     *
+     * @return
+     */
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", projectName);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Prometheusdemo2Application.class, args);
